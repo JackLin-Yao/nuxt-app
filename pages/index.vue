319e51be4c8e07d7da7578c00b3d4cee1d0c9e77
JackLin-Yao <jacklinyao0702@163.com>
2022-10-20 13:25:31 +0800

    carousel-components

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,33 @@
 <template>
-  <div >
-
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <side-menu></side-menu>
+      </el-col>
+      <el-col :span="19">
+        <life></life>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <artistic></artistic>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script >
-// import Index from '@/components/public/header/index.vue'
+import SideMenu from '@/components/index/menu.vue'
+import life from '@/components/index/life.vue';
+import artistic from '@/components/index/artistic.vue';
 
 export default {
   components: {
-    // Index
+    SideMenu, life, artistic
   }
 }
 </script>
 
 <style lang="scss">
+@import "@/assets/css/index/index.scss";
 </style>
